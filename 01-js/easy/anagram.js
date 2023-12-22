7d116ff7635509b838fc013bb81bbd515072839b
sanjeevs9 <sanjeev.19kr@gmail.com>
2023-12-22 11:59:28 +0530

    done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,23 @@
 */
 
 function isAnagram(str1, str2) {
+str1 = str1.toLowerCase();
+str2 = str2.toLowerCase();
+if(str1.length!==str2.length){
+  return false;
+}
+for(let i=0;i<str1.length;i++){
+   let char = str1.charAt(i);
+   let index = str2.indexOf(char);
+
+   if(index === -1){
+    return false;
+   }
+   
+   str2 = str2.slice(0,index) + str2.slice(index +1);
+
+}
+return true;
 
 }
 
